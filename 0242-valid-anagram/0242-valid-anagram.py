f1a3977389b07015a8afcfd6f72837e4458a601e
Nimisha Bhagat <51924838+nimisha218@users.noreply.github.com>
2023-10-03 10:02:44 -0400

    Time: 72 ms (6.34%), Space: 16.7 MB (95.41%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,11 +1,12 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        
+        if len(s) != len(t):
+            return False
 
         s_dict = {}
         t_dict = {}
 
-        if len(s) != len(t):
-            return False
 
         for i in range(len(s)):
             if s[i] not in s_dict:
